28c5b2f033e2d423d6e49702229032d791867281
Ryoichi Obara <ryoichi-obara@users.noreply.github.com>
2019-07-01 17:45:08 +0900

    Send msg when touchend.

--- README.md
+++ README.md
@@ -1,17 +1,8 @@
-# node-red-contrib-nfcpy-id
+# node-red-contrib-nfcpy-id-with-touchend
 
-A collection of [Node-RED](https://nodered.org) nodes for [nfcpy-id](https://github.com/mascii/node-nfcpy-id).
-Read the card ID (Mifare: UID, FeliCa: IDm) with `node-nfcpy-id`.
-Before using, install `nfcpy-id`(a Python module).
+Original  
+[https://github.com/k-harada413/node-red-contrib-nfcpy-id](https://github.com/k-harada413/node-red-contrib-nfcpy-id)
 
-## install
+## Updated from original
 
-Run the following command in the root directory of your Node-RED install
-
-```
-npm install node-red-contrib-nfcpy-id
-```
-
-## License
-See [license](https://github.com/k-harada413/node-red-contrib-nfcpy-id/blob/master/LICENSE) (Apache License Version 2.0).
-
+* Send msg when touchend.
--- nfcpy-id.js
+++ nfcpy-id.js
@@ -4,8 +4,10 @@ module.exports = function(RED) {
     function nfcpyidNode(n) {
         RED.nodes.createNode(this,n);
         this.waitTime = n.waitTime * 1000;
+        var tmp_card_id   = '';
+        var tmp_card_type = '';
         var node = this;
-        var nfc = new nfcpyid({mode:'non-touchend'}).start();
+        var nfc = new nfcpyid().start();
         this.status({fill:"green",shape:"ring",text:"waiting"});
         nfc.on('touchstart', (card) => { 
             try{
@@ -18,9 +20,11 @@ module.exports = function(RED) {
                 var msg = {
                     'payload': card.id,
                     'type': card.type,
+                    'status' : 1,
                     'timestamp': Date.now()
                 };
-
+                tmp_card_id = card.id;
+                tmp_card_type = card.type;
                 node.send(msg);
             }catch(err){
                 console.log("touch error");
@@ -28,13 +32,36 @@ module.exports = function(RED) {
             }
         });
 
+        nfc.on('touchend', () => {
+            try{
+                this.status({fill:"green",shape:"dot",text:"touchend"});
+
+                var msg = {
+                    'payload': tmp_card_id,
+                    'type': tmp_card_type,
+                    'status' : 0,
+                    'timestamp': Date.now()
+                };
+                
+                node.send(msg);
+            }catch(err){
+                console.log("touchend error");
+                restart_nfc();
+            }
+            tmp_card_id = '';
+            tmp_card_type = '';
+        });
+          
         nfc.on('error', (err) => {
-            // standard error output (color is red)
             console.error('\u001b[31m', err, '\u001b[0m');
+            tmp_card_id = '';
+            tmp_card_type = '';
         });
 
         node.on('close',function(){
             nfc.pause();
+            tmp_card_id = '';
+            tmp_card_type = '';
         });
     }
     RED.nodes.registerType("nfcpy-id",nfcpyidNode);
@@ -42,6 +69,8 @@ module.exports = function(RED) {
 
     function restart_nfc(){
         console.log("nfcerror and restartnfc");
+        tmp_card_id = '';
+        tmp_card_type = '';
         nfc.pause();
         nfc.start();
     }
